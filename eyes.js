de51fe7dc314f2a4239de3f8bbb606c93ec72b54
Varangian-core <victor.vladimir.mikhaltsevich@gmail.com>
2022-03-24 00:17:03 -0700

    Nightly Update

--- eyes.js
+++ eyes.js
@@ -3,9 +3,9 @@ document.onmousemove = (event) => {
     const x = (event.clientX * 100) / window.innerWidth + '%';
     const y = (event.clientY * 100) / window.innerHeight + '%';
 
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 4; i++) {
         balls[i].style.left = x;
         balls[i].style.top = y;
         balls[i].transform = 'translate(-' + x + ',-' + y + ')';
-    }
+    };
 };
